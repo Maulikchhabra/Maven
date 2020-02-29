281977d5af8c6c7afe67736433cfd415faaedd41
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-29 14:17:10 +0530

    Test class embedded with JUnit

---
+++ Lab2/test2.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test2 {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		Myclass ob =new Myclass();
+		assertEquals(5, ob.sum(3, 2));
+	}
+
+}
