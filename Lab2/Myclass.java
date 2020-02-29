56090e02fcb60b12ba17f5361ba6f3586646b790
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-29 14:16:19 +0530

    Main sample class to do JUnit testing upon

---
+++ Lab2/Myclass.java
@@ -0,0 +1,16 @@
+
+public class Myclass {
+	
+	public int sum(int a, int b){
+		int c=0;
+		c=a+b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Myclass ob =new Myclass();
+		System.out.println(ob.sum(2, 3));
+
+	}
+
+}
