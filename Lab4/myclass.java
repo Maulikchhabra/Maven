106be0cf93414f98302b90c62f27242f9e246f54
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-20 22:26:41 +0530

    Create myclass.java

---
+++ Lab4/myclass.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class myclass {
+     public int sum(int a,int b){
+    	 return (a+b);
+     }
+    public static void main(String []args){
+    	myclass ob = new myclass();
+    	ob.sum(2, 5);
+    }
+}
